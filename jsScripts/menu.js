d305e8d542871e170fe881fc9f56bf6806695987
Blessman-Newton <blessmannewton0@gmail.com>
2023-05-23 07:04:06 +0100

    Udate hamburger menu

--- jsScripts/menu.js
+++ jsScripts/menu.js
@@ -1,12 +1,20 @@
 const burger = document.querySelector('.burger');
 const desktopNav = document.querySelector('.desktop-nav');
+const display_nav = document.querySelector('header');
+
 
 burger.addEventListener('click', () => {
     burger.classList.toggle('active');
-    desktopNav.classList.toggle('active');
-})
+    desktopNav.classList.toggle('active'); 
+    display_nav.style.background = '#6070ff';
 
-document.querySelectorAll('.nav-link').forEach(n => n.
-    addEventListener('CLICK', () => {
-        burger.classList.remove('active')
-    }))
+});
+
+document.querySelectorAll('.nav-items').forEach(n => n.
+    addEventListener('click', () => {
+        burger.classList.remove('active');
+        desktopNav.classList.remove('active');
+        display_nav.style.background = '#fff';
+
+    }));
+    
