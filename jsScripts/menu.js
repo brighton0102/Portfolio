a33cbe8ca93a12994b8eb93f03f87942a5f7f626
brighton0102 <mtandibrighton0102@gmail.com>
2023-05-23 18:37:11 +0200

    added background styles to the close menu button

--- jsScripts/menu.js
+++ jsScripts/menu.js
@@ -1,14 +1,6 @@
 const burger = document.querySelector('.burger');
 const desktopNav = document.querySelector('.desktop-nav');
 
-const btn = document.getElementById('btn');
-
-btn.addEventListener('click', () => {
-  // display_nav.style.backgroundColor = '#fff';
-  // display.style.backgroundColor = '#fff';
-  btn.style.color = 'white';
-});
-
 burger.addEventListener('click', () => {
   burger.classList.toggle('active');
   desktopNav.classList.toggle('active');
@@ -18,5 +10,4 @@ document.querySelectorAll('.nav-items').forEach((n) => n
   .addEventListener('click', () => {
     burger.classList.remove('active');
     desktopNav.classList.remove('active');
-    /*  display_nav.style.background = '#fff'; */
   }));
