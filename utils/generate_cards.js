ca39e2fc23b40694260ebe39f4c358828a2ac52b
Blessman-Newton <blessmannewton0@gmail.com>
2023-05-25 09:45:40 +0100

    Modify the generate_cards.js file

---
+++ utils/generate_cards.js
@@ -0,0 +1,45 @@
+async function api() {
+    const response = await fetch("./utils/portfolio.json");
+    const jsonData = await response.json();
+ 
+    jsonData.data.portfolio.forEach((el, index) => {
+        const card_template = `
+            <section class="cards">
+                <div class=${(index + 1) % 2 === 0 ? `card-img-left` : `card-img-right` }>
+                    <img
+                    class="card-works-image"
+                    src=${el.featured_image}
+                    alt=${el.name+index}
+                    />
+                </div>
+                <div class=${(index + 1) % 2 == 0 ? `"card-work-info info-right" `: `"info-left card-work-info"`}>
+                    <h2 class="title-work-section">${el.name}</h2>
+                    <div class="tech-field">
+                        <p id="bold-st">${el.subheading[0]}</p>
+                        <div class="dot"></div>
+                        <p>${el.subheading[1]}</p>
+                        <div class="dot"></div>
+                        <p>${el.subheading[2]}</p>
+                    </div>
+                    <p class="description">
+                        ${el.description}
+                    </p>
+                    <ul class="languages-used">
+                    ${el.technology.map((res) => {
+                        return (`<li class="language">${ res}</li>`)
+                    })}
+                    </ul>
+                    <div>
+                    <a class="card-work-info" href="#">See project</a>
+                    </div>
+                </div>
+            </section>
+        `;
+
+        const element = document.querySelector('.card-works');
+        element.innerHTML += card_template;
+
+    });
+}
+
+api();
